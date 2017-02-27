e77623287d63c21db1e14247a1c5ade21d269ebb
Eric Bell <ewbell94@gmail.com>
2017-02-27 13:25:00 -0500

    Commenting files

--- bash/fetchlib.sh
+++ bash/fetchlib.sh
@@ -3,10 +3,12 @@
 #10/13/16
 #Fetchlib fetches tar files from the sciurus server and unzips them
 
+#download each file from sciurus
 for arg in "$@"; do
     scp ebell@sciurus.hpc.oberlin.edu:~/zinc/${arg}.tar.gz ~/ZINC
 done
 
+#unzip each file
 for arg in "$@"; do
     for thing in ${arg}; do
 	tar -zxf ~/ZINC/${thing}.tar.gz
--- python/randomset.py
+++ python/randomset.py
@@ -1,3 +1,8 @@
+#Written by Eric Bell
+#1/18/17
+#
+#randomset.py generates random sets of molecules from a res-like file
+
 from sys import argv
 import random
 
@@ -6,20 +11,21 @@ if len(argv) > 4 or len(argv) < 3:
     exit(1)
 
 try:
-    f=open(argv[1],"r")
+    f=open(argv[1],"r") #open res.txt file
 except:
     print("File not opened")
     exit(1)
 
-lines=f.readlines()
-random.shuffle(lines)
+lines=f.readlines() #get all lines of the file
+random.shuffle(lines) #shuffle all lines of the file
 try:
-    itercount=int(argv[3])
+    itercount=int(argv[3]) #the amount of sets to be generated
 except:
     itercount=1
 
+#create each set from shuffled lines
 for i in range(0,itercount):
     w=open("set"+str(i)+".txt","w")
-    for j in range(0,int(argv[2])):
+    for j in range(0,int(argv[2])): #argument 2 is the size of the set
         w.write(lines[i*int(argv[2])+j])
     w.close()
